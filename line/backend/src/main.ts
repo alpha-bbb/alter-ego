671b4314f8eb5685b645a34da570bfa9694d1e04
silenvx <czvziwzx@gmail.com>
2024-11-29 15:20:35 +0900

    feat: docker環境構築

--- line/backend/src/main.ts
+++ line/backend/src/main.ts
@@ -1,14 +1,17 @@
-import express from 'express';
-import { webhookHandler } from './functions/webhook';
-import { config } from './config';
+import { config } from "@/config.js";
+import { webhookHandler } from "@/functions/webhook.js";
+import express from "express";
 
 export const app = express();
 
 app.use(express.json());
 
-app.get('/webhook', webhookHandler);
-app.post('/webhook', webhookHandler);
+app.get("/", (_, res) => {
+  res.status(200).send("Hello, World!");
+});
+app.get("/webhook", webhookHandler);
+app.post("/webhook", webhookHandler);
 
 app.listen(config.port, () => {
-    console.log(`http://localhost:${config.port}/`);
+  console.log(`http://localhost:${config.port}/`);
 });
