a1c2d3a2be269d515a2f2070985146de46cbc743
yaaasuu <anyongw01123@gmail.com>
2024-12-20 23:30:32 +0900

    feat: fetch line message

--- line/backend/src/functions/webhook.ts
+++ line/backend/src/functions/webhook.ts
@@ -1,10 +1,63 @@
 import { config } from "@/config.js";
 import { messagingApi } from "@line/bot-sdk";
 import type { Request, Response } from "express";
+import * as https from "https";
 
 const { MessagingApiClient } = messagingApi;
 
 const client = new MessagingApiClient(config.line.messagingApiClient);
+
+type User = {
+  user_id: string;
+  name: string; // ユーザー名
+};
+
+type TalkHistories = {
+date: string;
+user: User;
+message: string;
+};
+
+function parseTalkHistories(talk: string, hostUserName:string): TalkHistories[] {
+const rows = talk.split("\n");
+const TalkHistories: TalkHistories[] = [];
+let talkDate: string | null = null;
+
+rows.forEach(row => {
+  const trimmedRow = row.trim();
+  // 日付
+  const dateMatch = trimmedRow.match(/^(\d{4}\/\d{2}\/\d{2})/);
+  if (dateMatch) {
+  talkDate = dateMatch[1].replace(/\//g, "-"); // YYYY-MM-DD
+  return;
+  }
+
+  // メッセージ（例: "22:07   Test    おはよう"）
+  const messageMatch = trimmedRow.match(/^(\d{2}:\d{2})\t+([^\t]+)?\t+(.+)$/);
+  if (messageMatch && talkDate) {
+      const [_, time, userName, message] = messageMatch;
+      const dateTime = `${talkDate}T${time}::00+0900`; // ISO 8601形式
+
+      const name = userName || "Unknown";
+      const user_id =
+      name === hostUserName
+        ? `${hostUserName}01`
+        : name === "Unknown"
+        ? "Unknown"
+        : `${name}02`;
+
+      TalkHistories.push({
+      date: dateTime,
+      user: { name, user_id },
+      message,
+      });
+  }else {
+      console.log("cannot parse");
+  }
+});
+
+return TalkHistories;
+}
 
 export const webhookHandler = async (
   req: Request,
@@ -19,14 +72,58 @@ export const webhookHandler = async (
             replyToken: e.replyToken,
             messages: [
               { type: "text", text: e.message.text },
-              {
-                type: "text",
-                text: "https://liff.line.me/2006595464-xg40E95E",
-              },
             ],
           });
         }
-      });
+
+
+      if (e.type === 'message' && e.message.type === 'file') {
+        console.log('res:', e);
+        try {
+            const endpoint = `https://api-data.line.me/v2/bot/message/${e.message.id}/content`
+            console.log("env token", config.line.messagingApiClient.channelAccessToken)
+            const response = await fetch(endpoint, {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${config.line.messagingApiClient.channelAccessToken}`
+                }
+            });
+            if (!response.ok) {
+                throw new Error(`Failed to fetch content: ${response.statusText}`);
+            }
+            const buffer = await response.arrayBuffer();
+            console.log('fileRes:', response);
+
+            const decoder = new TextDecoder('utf-8');
+            const talk = decoder.decode(buffer);
+            console.log('file contents:', talk);
+            const match = talk.match(/\[LINE\] (.*?)とのトーク履歴/);
+            let hostUserName = "noName";
+            if (match && match[1]) {
+                hostUserName = match[1];
+                console.log("Hostname:",hostUserName);
+            }
+
+            const TalkHistories = parseTalkHistories(talk, hostUserName);
+            console.log('TalkHistories:' ,TalkHistories);
+            if (TalkHistories){
+                const options = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                };
+                // TODO: POST先欲しいです
+                const url = "https://hogehoge"
+                const request = https.request(url, options);
+                request.write(TalkHistories);
+                request.end();
+            }
+        } catch (e) {
+            console.log('Error', e);
+        }
+    }
+  });
 
       await Promise.all(eventPromises);
     }
