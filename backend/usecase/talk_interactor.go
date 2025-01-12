c064e1295c1de3171b9c4b878ccba2f725a8e361
silenvx <czvziwzx@gmail.com>
2025-01-12 16:30:51 +0900

    fix: print

--- backend/usecase/talk_interactor.go
+++ backend/usecase/talk_interactor.go
@@ -18,15 +18,20 @@ type TalkUseCase interface {
 type talkInteractor struct {
 	llmClient server.LLMClient
 }
+
 func NewTalkInteractor(llmClient server.LLMClient) TalkUseCase {
 	return &talkInteractor{llmClient: llmClient}
 }
 
 func (i *talkInteractor) HandleTalk(ctx context.Context, req *backendpb.TalkRequest) (*backendpb.TalkResponse, error) {
 	entityTalkHistory := convert.ConvertTalkHistoryFromGRPCTalkRequest(req)
+	fmt.Print("🔴")
+	fmt.Print(entityTalkHistory)
 	llmHistories := convert.ConvertTalkHistoryToGRPCTalkResponse(entityTalkHistory)
 	llmRequest := &llmpb.TalkRequest{Histories: llmHistories}
 	llmResponse, err := i.llmClient.Talk(ctx, llmRequest)
+	fmt.Print("🟢")
+	fmt.Print(llmResponse)
 	if err != nil {
 		return nil, fmt.Errorf("failed to call LLM service: %w", err)
 	}
