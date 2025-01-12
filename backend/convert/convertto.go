f6e1048efcf23bf35c4e85d0547168086c59515a
silenvx <czvziwzx@gmail.com>
2025-01-12 21:20:09 +0900

    Revert "FIX: protoスキーマーに Userのme, youを追加"

--- backend/convert/convertto.go
+++ backend/convert/convertto.go
@@ -6,21 +6,20 @@ import (
 )
 
 func ConvertTalkHistoryToGRPCTalkResponse(histories []*entity.TalkHistory) []*llmpb.TalkHistory {
-	if histories == nil {
-		return nil
-	}
+    if histories == nil {
+        return nil
+    }
 
-	result := make([]*llmpb.TalkHistory, len(histories))
-	for i := range histories {
-		result[i] = &llmpb.TalkHistory{
-			Date: histories[i].Date,
-			User: &llmpb.User{
-				UserId: histories[i].User.UserID,
-				Name:   histories[i].User.Name,
-				Role:   llmpb.UserUserRole(histories[i].User.Role),
-			},
-			Message: histories[i].Message,
-		}
-	}
-	return result
+    result := make([]*llmpb.TalkHistory, len(histories))
+    for i := range histories {
+        result[i] = &llmpb.TalkHistory{
+            Date: histories[i].Date,
+            User: &llmpb.User{
+                UserId: histories[i].User.UserID,
+                Name:   histories[i].User.Name,
+            },
+            Message: histories[i].Message,
+        }
+    }
+    return result
 }
