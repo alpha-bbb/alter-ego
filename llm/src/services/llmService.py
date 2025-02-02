e55631f34bf0f5e3db6a1b40af102ff690e699de
Yang Jingtao <83807321+You-keitou@users.noreply.github.com>
2025-02-03 02:38:02 +0900

    feat: langgraphによるllm talkサービスの実装

--- llm/src/services/llmService.py
+++ llm/src/services/llmService.py
@@ -1,9 +1,11 @@
 import logging
-from typing import List
 
 import grpc
 from llm.v1 import llm_pb2, llm_pb2_grpc
-from src.services.external_api.dify import call_dify_talk
+from google.protobuf.json_format import MessageToDict
+# from src.services.external_api.dify import call_dify_talk
+from src.core.graph import graph
+from src.core.model import TalkHistory
 
 logger = logging.getLogger(__name__)
 
@@ -29,17 +31,14 @@ class LlmServiceServicer(llm_pb2_grpc.LlmServiceServicer):
         """
         try:
             # 履歴を300件に制限
-            histories = (
-                request.histories[-300:]
-                if len(request.histories) > 300
-                else request.histories
-            )
+            histories = [TalkHistory(**MessageToDict(history)) for history in request.histories[-300:]]
 
             # Dify APIを呼び出し
-            messages = await call_dify_talk(histories)
+            # messages = await call_dify_talk(histories)
+            messages = await graph.ainvoke({ "talk_histories": histories })
 
             # レスポンスの作成
-            return llm_pb2.TalkResponse(message=messages["replies"])
+            return llm_pb2.TalkResponse(message=messages["final_responses"])
 
         except Exception as e:
             logger.error(f"Error in Talk: {str(e)}", exc_info=True)
